c633424cdaec9b7a840f427d272fb29a43b8a34b
JeydenJ <Jeyden.Johnson-1@student.uts.edu.au>
2024-09-18 16:23:51 +1000

    added some fixes to footer

--- header.js
+++ header.js
@@ -10,7 +10,6 @@ document.addEventListener("DOMContentLoaded", () => {
         'contact.html': 'Contact Me'
     };
 
-    // Load and update header content
     const loadHeaderContent = async () => {
         try {
             const response = await fetch(headerUrl);
@@ -18,24 +17,20 @@ document.addEventListener("DOMContentLoaded", () => {
             const data = await response.text();
             document.querySelector(headerSelector).innerHTML = data;
 
-            // Set page-specific title
             updateHeaderTitle();
         } catch (error) {
             console.error('Error loading header:', error);
         }
     };
 
-    // Update header title based on the current page
     const updateHeaderTitle = () => {
         const currentPage = window.location.pathname.split('/').pop();
         const headerTitle = document.querySelector('.header-title h1');
         if (headerTitle) {
-            headerTitle.innerHTML = pageTitles[currentPage] || 'Default Title';
+            headerTitle.innerHTML = pageTitles[currentPage] || 'Hi, I am Jeyden Johnson.<br>I do Web Development.';
         } else {
             console.error('Header title element not found.');
         }
     };
-
-    // Initialize
     loadHeaderContent();
 });
